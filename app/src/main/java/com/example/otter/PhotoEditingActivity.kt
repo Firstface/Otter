2f075972325057aa1901e25f0b26aabedaf1c3d0
V_E <E1391157@u.nus.edu>
2025-11-25 20:42:42 +0800

    feat: 初步实现醒图仿版首页UI，主题配置和修图操作界面
    主要变更：
    1. 新增 fragment_home.xml 布局
       - 实现了顶部的“导入”与“马上开拍”大按钮布局
       - 实现了中间的功能卡片区及网格工具栏
       - 实现了底部的自定义 Tab 导航栏
    2. 更新 colors.xml
       - 添加品牌色 neon_green (#CCFF00)
       - 添加浅色背景 light_green (#F1F8D8)
    3. 优化 themes.xml
       - 配置 NoActionBar 主题以支持自定义标题栏
       - 开启 windowLightStatusBar 适配白色背景
    4. 新增 Drawable 资源
       - bg_home_camera.xml (荧光绿按钮背景)
       - bg_home_import.xml (黑色按钮背景)
    
    代办事项:
    1. 完善醒图首页，将写死功能区修改为recycleViewer。
    2. 添加点击跳转，实现页面互联

--- app/src/main/java/com/example/otter/PhotoEditingActivity.kt
+++ app/src/main/java/com/example/otter/PhotoEditingActivity.kt
@@ -4,11 +4,10 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
+class PhotoEditingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_photoediting)
     }
 }
